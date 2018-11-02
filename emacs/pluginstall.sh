5d5f2c14a9b002ea5c2cd9cfb70e51c5fd6ff6a6
Shuai-Hsun <hellovictorlee@gmail.com>
2018-11-02 02:38:22 -0700

    add: emacs package install

---
+++ emacs/pluginstall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git clone https://github.com/emacs-evil/evil ~/.emacs.d/evil
+git clone https://github.com/n3mo/cyberpunk-theme.el.git ~/.emacs.d/cyberpunk-theme.el
+git clone https://github.com/dbordak/telephone-line.git ~/.emacs.d/telephone-line
+git clone https://github.com/nonsequitur/smex.git ~/.emacs.d/smex
