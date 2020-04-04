c8435166f412483b9d73080c85ec7d7d32542c4d
Victor <hellovictorlee@gmail.com>
2020-04-04 01:16:31 -0700

    update

---
+++ i3-family/rofi/rofi-bangs.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+# author: unknown
+# sentby: MoreChannelNoise (https://www.youtube.com/user/MoreChannelNoise)
+# editby: gotbletu (https://www.youtube.com/user/gotbletu)
+
+# demo: https://www.youtube.com/watch?v=kxJClZIXSnM
+# info: this is a script to launch other rofi scripts,
+#       saves us the trouble of binding multiple hotkeys for each script,
+#       when we can just use one hotkey for everything.
+
+declare -A LABELS
+declare -A COMMANDS
+
+###
+# List of defined 'bangs'
+
+# launch programs
+COMMANDS["apps"]="rofi -show drun -theme onedark"
+LABELS["apps"]=""
+
+# open bookmarks
+COMMANDS["bookmarks"]="~/.scripts/rofi-scripts-collection/rofi-surfraw-bookmarks.sh"
+LABELS["bookmarks"]=""
+
+# calculator
+COMMANDS["calculator"]="rofi -show calc -theme onedark"
+LABELS["calculator"]=""
+
+# greenclip
+COMMANDS["clipboard"]="rofi -modi \"clipboard:greenclip print\" -show clipboard -theme onedark"
+LABELS["clipboard"]=""
+
+# open bluetooth
+COMMANDS["bluetooth"]="~/.config/rofi/rofi-bluetooth"
+LABELS["bluetooth"]=""
+
+# search local files
+COMMANDS["favorite"]="~/.config/rofi/rofi-surfraw.sh"
+LABELS["favorite"]=""
+
+# open custom web searches
+COMMANDS["websearch"]="rofi -show search -theme onedark -modi search:~/.config/rofi/rofi-web-search.py"
+LABELS["websearch"]=""
+
+# open wifi
+COMMANDS["wifi"]="~/.config/rofi/rofi-wifi-menu.sh"
+LABELS["wifi"]=""
+
+
+# show clipboard history
+# source: https://bitbucket.org/pandozer/rofi-clipboard-manager/overview
+# COMMANDS["clipboard"]='rofi -modi "clipboard:~/.bin/rofi-clipboard-manager/mclip.py menu" -show clipboard && ~/.bin/rofi-clipboard-manager/mclip.py paste'
+# LABELS["clipboard"]=""
+
+# references --------------------------
+# COMMANDS[";sr2"]="chromium 'wikipedia.org/search-redirect.php?search=\" \${input}\""
+# LABELS[";sr2"]=""
+
+# COMMANDS[";piratebay"]="chromium --disk-cache-dir=/tmp/cache http://thepiratebay.org/search/\" \${input}\""
+# LABELS[";piratebay"]=""
+
+# COMMANDS[".bin"]="spacefm -r '/home/dka/bin'"
+# LABELS[".bin"]=".bin"
+
+# COMMANDS["#screenshot"]='/home/dka/bin/screenshot-scripts/myscreenshot.sh'
+# LABELS["#screenshot"]="screenshot"
+
+################################################################################
+# do not edit below
+################################################################################
+##
+# Generate menu
+##
+function print_menu()
+{
+    for key in ${!LABELS[@]}
+    do
+  echo "$key    ${LABELS}"
+     #   echo "$key    ${LABELS[$key]}"
+     # my top version just shows the first field in labels row, not two words side by side
+    done
+}
+##
+# Show rofi.
+##
+function start()
+{
+    # print_menu | rofi -dmenu -p "?=>" 
+    print_menu | sort | rofi -dmenu -i -p "rofi-bangs: " -theme onedark
+
+}
+
+
+# Run it
+value="$(start)"
+
+# Split input.
+# grab upto first space.
+choice=${value%%\ *}
+# graph remainder, minus space.
+input=${value:$((${#choice}+1))}
+
+##
+# Cancelled? bail out
+##
+if test -z ${choice}
+then
+    exit
+fi
+
+# check if choice exists
+if test ${COMMANDS[$choice]+isset}
+then
+    # Execute the choice
+    eval echo "Executing: ${COMMANDS[$choice]}"
+    eval ${COMMANDS[$choice]}
+else
+ eval  $choice | rofi
+ # prefer my above so I can use this same script to also launch apps like geany or leafpad etc (DK) 
+ #   echo "Unknown command: ${choice}" | rofi -dmenu -p "error"
+fi
